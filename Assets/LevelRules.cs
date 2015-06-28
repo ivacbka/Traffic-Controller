f6641a44cfcc6a792ac66a90bdee38a7ed8765d0
ivacbka <pol-ii@ya.ru>
2015-06-28 16:08:33 +0300

    added unity project
    initial

---
+++ Assets/LevelRules.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class LevelRules : MonoBehaviour 
+{
+	private List<Car>	_cars;
+
+	public void CarSpawned(GameObject car, gameNode target, gameNode source)
+	{
+		_cars.Add(car.GetComponent<Car>); 
+	}
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
---
+++ Assets/LevelSetup.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class LevelSetup : MonoBehaviour 
+{
+	public GameObject CarPrefab;
+	public List<GameNode> Sources;
+	public List<GameNode> Sinks;
+	public int CarsAmount;
+	public float SpawnPeriod;
+
+	private bool _spawnFinished;
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+
+	public IEnumerator SpawnCoroutine(float period)
+	{
+		int spawned = 0;
+
+		while(true)
+		{
+			yield return new WaitForSeconds(SpawnPeriod);
+			SpawnCar(Sources[UnityEngine.Random.Range(0, Sources.Count)]);
+			spawned++;
+
+			if(CarsAmount == spawned)
+				break;
+		}
+	}
+
+	public void SpawnCar(GameNode node)
+	{
+		GameObject.Instantiate(CarPrefab, node.transform.position, node.transform.rotation);
+
+	}
+	// Update is called once per frame
+	void Update () 
+	{
+
+
+	}
+}
